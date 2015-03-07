8e58e7bcb65f2fc2d238aab0db32daca97819da2
Scott Fleckenstein <nullstyle@gmail.com>
2015-03-06 16:14:39 -0800

    Cleanup:
    - Remove unused templates
    - Remove improper rescue_from, now that we use a middleware
    - Remove controller specs that should be middleware specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  rescue_from StandardError, with: :render_error
-
-  private
-  def render_error(err)
-    self.response_body = nil # reset the rendering if we had a response body
-    render(problem:err)
-  end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,15 +4,7 @@ RSpec.describe ApplicationController, type: :controller do
   controller do
     before_action :add_request_id
 
-    def problem_via_exception
-      raise "Explode!"
-    end
-
-    def problem_via_custom_exception
-      raise AccessDeniedError
-    end
-
-    def problem_via_hash
+    def index
       return render(problem: {
         type: "out_of_space",
         requested_space: "100G",
@@ -28,46 +20,18 @@ RSpec.describe ApplicationController, type: :controller do
     end
   end
 
-  before(:each) do
-    routes.draw { get ":action", controller: "anonymous" }
-  end
-
   describe "Problem Rendering" do
+    before(:each){ get :index }
+
     it "sets the 'type' field to a url into our error documentation"
     
-    it "renders generic exceptions as an 'Internal Server Error' problem" do
-      get :problem_via_exception
-      expect(response).to match_json({
-        title: "Internal Server Error"
-      })
-    end
-
-    it "renders exceptions that override to_problem based on the override" do
-      get :problem_via_custom_exception
-      expect(response).to match_json({
-        title: "Access Denied"
-      })
-    end
-
-    it "defaults to 500 status code" do
-      get :problem_via_exception
-      expect(response).to have_status(:internal_server_error)
-    end
-
-    it "allows the provided problem to override the status code" do
-      get :problem_via_custom_exception
-      expect(response).to have_status(:forbidden)
-    end
-    
     it "includes the request UUID in a URI as the 'instance' field" do
-      get :problem_via_hash
       expect(response).to match_json({
         instance: "request://123"
       })
     end
 
     it "includes extra fields defined on the problem hash in the response" do
-      get :problem_via_hash
       expect(response).to match_json({
         requested_space: "100G",
         available_space: "100M",
