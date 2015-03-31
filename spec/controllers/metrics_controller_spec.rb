faa08c2d89a6780427f52826ce25a8a19abca0c9
Scott Fleckenstein <nullstyle@gmail.com>
2015-03-31 15:19:39 -0700

    Fix metrics spec

--- spec/controllers/metrics_controller_spec.rb
+++ spec/controllers/metrics_controller_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe MetricsController, type: :controller do
     before(:each){ get :index, format: :json }
 
     it "renders the index template" do
-      expect(response).to render_template("index")
+      expect(response).to match_json({
+        "_links" => {"self" => Hash},
+
+        "requests.total"        => Hash,
+        "requests.succeeded"    => Hash,
+        "requests.failed"       => Hash,
+        "celluloid.actor_count" => Fixnum,
+      })
     end
 
     it "succeeds" do
