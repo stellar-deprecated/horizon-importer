80e73238db05aefe28a5089321600262ff2497d9
Scott Fleckenstein <nullstyle@gmail.com>
2015-05-01 14:34:15 -0700

    Fix migration

--- db/migrate/20150501160031_add_order.rb
+++ db/migrate/20150501160031_add_order.rb
@@ -1,11 +1,11 @@
 class AddOrder < ActiveRecord::Migration
   def change
-    add_column :history_ledgers, :order, :integer, limit:8, null: false
-    add_column :history_transactions, :order, :integer, limit:8, null: false
+    add_column :history_ledgers, :order, :integer, limit:8
+    add_column :history_transactions, :order, :integer, limit:8
 
     add_index :history_ledgers, :order, unique: true
     add_index :history_transactions, :order, unique: true
-    
+
     remove_column :history_ledgers, :id
     remove_column :history_transactions, :id
   end
