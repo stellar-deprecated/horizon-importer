4e3507298a0d94a6442d6459100a88d0ab4a3c19
Scott Fleckenstein <nullstyle@gmail.com>
2015-06-29 08:19:39 -0700

    Add BTC offers to order_books scenario

--- spec/fixtures/scenarios/order_books.rb
+++ spec/fixtures/scenarios/order_books.rb
@@ -16,10 +16,16 @@ close_ledger
 trust :scott,  :usd_gateway, "USD"
 trust :andrew, :usd_gateway, "USD"
 
+trust :scott,  :usd_gateway, "BTC"
+trust :andrew, :usd_gateway, "BTC"
+
 close_ledger
 
 payment :usd_gateway, :scott,   ["USD", :usd_gateway, 5000]
 payment :usd_gateway, :andrew,  ["USD", :usd_gateway, 5000]
+
+payment :usd_gateway, :scott,   ["BTC", :usd_gateway, 5000]
+payment :usd_gateway, :andrew,  ["BTC", :usd_gateway, 5000]
 
 close_ledger
 
@@ -30,3 +36,13 @@ offer :scott, {buy:["USD", :usd_gateway], with: :native}, 1000, 5.0
 offer :andrew, {sell:["USD", :usd_gateway], for: :native}, 10, 15.0
 offer :andrew, {sell:["USD", :usd_gateway], for: :native}, 100, 20.0
 offer :andrew, {sell:["USD", :usd_gateway], for: :native}, 1000, 50.0
+
+close_ledger
+
+offer :scott, {buy:["USD", :usd_gateway], with: ["BTC", :usd_gateway]}, 10, 10.0
+offer :scott, {buy:["USD", :usd_gateway], with: ["BTC", :usd_gateway]}, 100, 9.0
+offer :scott, {buy:["USD", :usd_gateway], with: ["BTC", :usd_gateway]}, 1000, 5.0
+
+offer :andrew, {sell:["USD", :usd_gateway], for: ["BTC", :usd_gateway]}, 10, 15.0
+offer :andrew, {sell:["USD", :usd_gateway], for: ["BTC", :usd_gateway]}, 100, 20.0
+offer :andrew, {sell:["USD", :usd_gateway], for: ["BTC", :usd_gateway]}, 1000, 50.0
